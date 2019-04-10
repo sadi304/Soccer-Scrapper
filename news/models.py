dbf4bbcc84516f7241e3ce2d5c538a361af3cc57
sadi304 <sadisagar@gmail.com>
2019-04-11 00:36:00 +0600

    Add: post model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  body = models.TextField()
+  created_at = models.DateTimeField('date published')
+  image_url = models.CharField(max_length=200)
+  def __str__(self):
+    return self.title
+
+# class Choice(models.Model):
+#   question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#   choice_text = models.CharField(max_length=200)
+#   votes = models.IntegerField(default=0)
