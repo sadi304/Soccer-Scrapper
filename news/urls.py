294b70cca40848482e3f744e34492b1ae35c75f3
sadi304 <sadisagar@gmail.com>
2019-04-15 00:40:28 +0600

    Add: css styles, views, templates

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'news'
 urlpatterns = [
   path('', views.index, name='index'),
+  path('<int:post_id>/', views.detail, name='detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+  latest_posts = Post.objects.order_by('created_at')
+  context = {'latest_question_list': latest_posts}
+  return render(request, './index.html', context)
+
+def detail(request, post_id):
+  try:
+    post = Post.objects.get(pk=post_id)
+  except Question.DoesNotExist:
+    raise Http404("Post does not exist")
+  return render(request, './detail.html', {'post': post})
